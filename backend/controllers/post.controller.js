ece00ce008125bc57bb1be97d178eb9a8153b792
Renaud97430 <rdh79@hotmail.fr>
2023-09-14 15:41:08 +0400

    ajout update/delete post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,9 +1,46 @@
 const PostModel = require("../models/post.model");
 
+module.exports.getPosts = async (req, res) => {
+        const posts = await PostModel.find();
+        res.status(200).json(posts)
+ };
+
+
+
 module.exports.setPosts = async (req, res) => {
     if (!req.body.message) {
     res.status(400).json({message: "Merci d'ajouter un message"})
     }
-    res.status(200).end();
+    
     // res.json(req.body);
+    const post = await PostModel.create({
+        message: req.body.message,
+        author: req.body.author
+    })
+    res.status(200).json(post)
 };
+
+module.exports.editPost = async (req, res) => {
+    const post = await PostModel.findById(req.params.id)
+    if (!post) {
+        res.status(400).json({message: "Ce post n'existe pas"})
+    }
+
+    const updatePost = await PostModel.findByIdAndUpdate(
+        post,
+        req.body,
+        {new: true}
+    )
+    res.status(200).json(updatePost);
+};
+
+module.exports.deletePost = async (req, res) => {
+    const post = await PostModel.findById(req.params.id);
+
+    if (!post) {
+        res.status(400).json({message: "Ce post n'existe pas"})
+    }
+
+    await PostModel.remove();
+    res.status(200).json("Message supprimé " + post);
+}
